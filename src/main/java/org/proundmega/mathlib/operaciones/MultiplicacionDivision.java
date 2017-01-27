438c186980311b04df27fa84b3269c0b304f014c
proundmega <proundmega@sakshi-PC.lan>
2017-01-27 14:35:57 -0600

    *** Las multiplicaciones funcionan ***
    Falta añadir testes que cubran tanto sumar como multiplicar.

--- src/main/java/org/proundmega/mathlib/operaciones/MultiplicacionDivision.java
+++ src/main/java/org/proundmega/mathlib/operaciones/MultiplicacionDivision.java
@@ -1,13 +1,33 @@
 package org.proundmega.mathlib.operaciones;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MultiplicacionDivision {
     private String formula;
+    
+    private static final Pattern pattern = Pattern.compile("\\*|/");
 
     public MultiplicacionDivision(String formula) {
         this.formula = formula;
     }
     
     public double operar() {
-        return 0;
+        Matcher matcher = pattern.matcher(formula);
+        String[] valores = pattern.split(formula);
+        
+        double suma = Double.parseDouble(valores[0]);
+        int actual = 1;
+        
+        while (matcher.find()) {
+            String operando = matcher.group();
+            double valorActual = Double.parseDouble(valores[actual]);
+            
+            suma = operando.equals("*") ? suma * valorActual : suma / valorActual;
+            actual++;
+        }
+        
+        return suma;
     }
+    
 }
--- src/test/java/org/proundmega/mathlib/operaciones/MultiplicacionDivisionTest.java
+++ src/test/java/org/proundmega/mathlib/operaciones/MultiplicacionDivisionTest.java
@@ -6,7 +6,64 @@ import static org.junit.Assert.*;
 public class MultiplicacionDivisionTest {
     
     @Test
-    public void testOperar() {
+    public void multiplicar2Valores() {
+        String prueba = "2*3";
+        double esperado = 6.0;
+        
+        multiplicarYAssert(prueba, esperado);
     }
     
+    private void multiplicarYAssert(String prueba, double esperado) {
+        MultiplicacionDivision md = new MultiplicacionDivision(prueba);
+        double obtenido = md.operar();
+        assertEquals(esperado, obtenido, 0.01);
+    }
+    
+    @Test
+    public void multiplicar3Valores() {
+        String prueba = "2*3*4";
+        double esperado = 24.0;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
+    
+    @Test
+    public void multiplicarVariosValoresConSigno() {
+        String prueba = "-2*-3*+4*5*-6";
+        double esperado = -720.0;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
+    
+    @Test
+    public void dividir2Valores() {
+        String prueba = "2/4";
+        double esperado = 0.5;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
+    
+    @Test
+    public void dividir3Valores() {
+        String prueba = "20/4/5";
+        double esperado = 1.0;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
+    
+    @Test
+    public void dividirVariosValoresConSigno() {
+        String prueba = "-250/+3/-2.44/+3.16";
+        double esperado = 10.80791312;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
+    
+    @Test
+    public void juntarTodo1() {
+        String prueba = "2.4*-6.2/41.3*7.14/3.15";
+        double esperado = -0.816658595;
+        
+        multiplicarYAssert(prueba, esperado);
+    }
 }
